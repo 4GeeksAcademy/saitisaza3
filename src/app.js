335aad127faf295db0dcc24e29a5f0430cfe3a91
saitisaza <156968617+saitisaza@users.noreply.github.com>
2024-02-17 01:09:31 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generatorExcuse();
+  });
 };
+
+function generatorExcuse() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoindx = Math.floor(Math.random() * who.length);
+  let actindx = Math.floor(Math.random() * action.length);
+  let whatindx = Math.floor(Math.random() * what.length);
+  let whenindx = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoindx] +
+    " " +
+    action[actindx] +
+    " " +
+    what[whatindx] +
+    " " +
+    when[whenindx]
+  );
+}
